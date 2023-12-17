17a758e0994efb33ad5a0782fe3489fd98374934
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2023-12-17 15:52:28 +0100

    HW

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,10 @@
 function checkForSpam(message) {
-  message.toLowerCase();
+  // message.toLowerCase();
 
-  if (message.includes('spam' || 'sale')) {
+  if (
+    message.toLowerCase().includes('spam') ||
+    message.toLowerCase().includes('sale')
+  ) {
     return true;
   } else {
     return false;
@@ -15,3 +18,4 @@ console.log(checkForSpam('Amazing SalE, only tonight!')); // true
 console.log(checkForSpam('Trust me, this is not a spam message')); // true
 console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
 console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
